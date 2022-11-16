814232e13d95f96fb932c5abc24c3cb0c0159a3b
Admin <Admin@Admin-PC>
2022-11-16 14:48:38 +0700

    Phu Xuan update

--- src/Function/WorkerThread.java
+++ src/Function/WorkerThread.java
@@ -3,11 +3,11 @@ package Function;
 import java.net.*;
 import java.io.*;
 
-public class WorkerThread extends Thread{
+public class WorkerThread extends Thread {
 	private InputStream inputStream = null;
 	private OutputStream outputStream = null;
 	private Socket clientSocket = null;
-	
+
 	public WorkerThread(Socket clientSocket) {
 		try {
 			this.clientSocket = clientSocket;
@@ -16,50 +16,43 @@ public class WorkerThread extends Thread{
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-	
+
 	}
-	
+
 	@Override
 	public void run() {
 		try {
+			ObjectOutputStream oos = new ObjectOutputStream(outputStream);
 			ObjectInputStream ois = new ObjectInputStream(inputStream);
 			String request = (String) ois.readObject();
-			if(request.equals("GET_PROCESS_LIST")) {
-			try {
-				ObjectOutputStream oos = new ObjectOutputStream(outputStream);
-				String[] commandList = {"powershell.exe", "-Command", "Get-Process"};
-				ProcessBuilder processBuilder = new ProcessBuilder(commandList);
-				Process list = processBuilder.start();
-				BufferedReader processBuffer = new BufferedReader(new InputStreamReader(list.getInputStream()));
-				String processLine = "";
-				String outData = "";
-				while((processLine = processBuffer.readLine() )!= null) {
-					outData += processLine + '\n';
+			if (request.equals("GET_PROCESS_LIST")) {
+				try {
+					ListProcess listProcess = new ListProcess(oos);
+					listProcess.set_listProcess();
+					listProcess.sendListProcess();
+				} catch (Exception e) {
+					e.getStackTrace();
 				}
-				System.out.println(outData);
-				oos.writeObject(outData);
-	            ois.close();
-	            oos.close();
-			} catch (Exception e) {
-				e.getStackTrace();
-			}
-		} else if(request.equals("GET_APPLICATION_LIST")) {
-			try {
-				String[] commandList = {"powershell.exe", "-Command", "gps | ? {$_.mainwindowtitle} | select name, id, mainwindowtitle | ft -AutoSize"};
-				ProcessBuilder processBuilder = new ProcessBuilder(commandList);
-				Process list = processBuilder.start();
-				BufferedReader processBuffer = new BufferedReader(new InputStreamReader(list.getInputStream()));
-				String processLine = "";
-				while((processLine = processBuffer.readLine()) != null) {
-					System.out.println(processLine);
-				}
-				
-			} catch (Exception e) {
-				e.getStackTrace();
-			}
-		} 
+			} else if (request.equals("GET_APPLICATION_LIST")) {
+				try {
+					String[] commandList = { "powershell.exe", "-Command",
+							"gps | ? {$_.mainwindowtitle} | select name, id, mainwindowtitle | ft -AutoSize" };
+					ProcessBuilder processBuilder = new ProcessBuilder(commandList);
+					Process list = processBuilder.start();
+					BufferedReader processBuffer = new BufferedReader(new InputStreamReader(list.getInputStream()));
+					String processLine = "";
+					while ((processLine = processBuffer.readLine()) != null) {
+						System.out.println(processLine);
+					}
 
-			
+				} catch (Exception e) {
+					e.getStackTrace();
+				}
+			} else if (request.equals("START_PROCESS")) {
+
+			}
+			oos.close();
+			ois.close();
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
