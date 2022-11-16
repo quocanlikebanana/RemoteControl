219e5f143d23de7e2ce1d393f17dac1f3c1c2150
Admin <Admin@Admin-PC>
2022-11-16 16:10:50 +0700

    Finish handling process on server (list, start, stop)

--- src/Function/WorkerThread.java
+++ src/Function/WorkerThread.java
@@ -24,13 +24,28 @@ public class WorkerThread extends Thread {
 		try {
 			ObjectOutputStream oos = new ObjectOutputStream(outputStream);
 			ObjectInputStream ois = new ObjectInputStream(inputStream);
+
 			String request = (String) ois.readObject();
-			if(request.equals("GET_PROCESS_LIST")) {
+			String[] request_elements = request.split(" ");
+			// gui request nhu sau: "KILL_PROCESS 12312"
+			// gui request nhu sau: "START_PROCESS pornhub.exe"
+
+			if (request.equals("GET_PROCESS_LIST")) {
 				ListProcess listProcess = new ListProcess(oos);
 				listProcess.set_listProcess();
 				listProcess.sendListProcess();
+			} else if (request.equals("KILL_PROCESS")) {
+				ProcessKill processKill = new ProcessKill(oos, request_elements[1]);
+				processKill.kill();
+				processKill.send_result();
+			} else if (request.equals("START_PROCESS")) {
+				ProcessStart processStart = new ProcessStart(oos, request_elements[1]);
+				processStart.start_file();
+				processStart.send_result();
+			}else {
+				// ......... them code vao day...........//
+				// last modyfied by phuxuan2k3 trainhaquedidepleuongcafe
 			}
-
 			oos.close();
 			ois.close();
 		} catch (Exception e) {
