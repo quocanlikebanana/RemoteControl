ac8ec1975eac5390b616a4b3d7a1c6268c2ff3eb
Boublet <65109951+Boublet@users.noreply.github.com>
2022-11-16 13:07:56 +0700

    Update

--- src/Function/WorkerThread.java
+++ src/Function/WorkerThread.java
@@ -1,11 +1,7 @@
 package Function;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.net.*;
+import java.io.*;
 
 public class WorkerThread extends Thread{
 	private InputStream inputStream = null;
@@ -13,50 +9,56 @@ public class WorkerThread extends Thread{
 	private Socket clientSocket = null;
 	
 	public WorkerThread(Socket clientSocket) {
-		this.clientSocket = clientSocket;
+		try {
+			this.clientSocket = clientSocket;
+			this.inputStream = clientSocket.getInputStream();
+			this.outputStream = clientSocket.getOutputStream();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	
 	}
 	
 	@Override
 	public void run() {
 		try {
-			inputStream = clientSocket.getInputStream();
-			outputStream = clientSocket.getOutputStream();
-			BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
-			String request = "";
-			while(in.ready()) {
-				request +=  String.valueOf((char)in.read());
-			}
-			System.out.println(request);
-			
+			ObjectInputStream ois = new ObjectInputStream(inputStream);
+			String request = (String) ois.readObject();
 			if(request.equals("GET_PROCESS_LIST")) {
-				try {
-					String[] commandList = {"powershell.exe", "-Command", "Get-Process"};
-					ProcessBuilder processBuilder = new ProcessBuilder(commandList);
-					Process list = processBuilder.start();
-					BufferedReader processBuffer = new BufferedReader(new InputStreamReader(list.getInputStream()));
-					String processLine = "";
-					while((processLine = processBuffer.readLine()) != null) {
-						System.out.println(processLine);
-					}
-					
-				} catch (Exception e) {
-					e.getStackTrace();
+			try {
+				ObjectOutputStream oos = new ObjectOutputStream(outputStream);
+				String[] commandList = {"powershell.exe", "-Command", "Get-Process"};
+				ProcessBuilder processBuilder = new ProcessBuilder(commandList);
+				Process list = processBuilder.start();
+				BufferedReader processBuffer = new BufferedReader(new InputStreamReader(list.getInputStream()));
+				String processLine = "";
+				String outData = "";
+				while((processLine = processBuffer.readLine() )!= null) {
+					outData += processLine + '\n';
 				}
-			} else if(request.equals("GET_APPLICATION_LIST")) {
-				try {
-					String[] commandList = {"powershell.exe", "-Command", "gps | ? {$_.mainwindowtitle} | select name, id, mainwindowtitle | ft -AutoSize"};
-					ProcessBuilder processBuilder = new ProcessBuilder(commandList);
-					Process list = processBuilder.start();
-					BufferedReader processBuffer = new BufferedReader(new InputStreamReader(list.getInputStream()));
-					String processLine = "";
-					while((processLine = processBuffer.readLine()) != null) {
-						System.out.println(processLine);
-					}
-					
-				} catch (Exception e) {
-					e.getStackTrace();
+				System.out.println(outData);
+				oos.writeObject(outData);
+	            ois.close();
+	            oos.close();
+			} catch (Exception e) {
+				e.getStackTrace();
+			}
+		} else if(request.equals("GET_APPLICATION_LIST")) {
+			try {
+				String[] commandList = {"powershell.exe", "-Command", "gps | ? {$_.mainwindowtitle} | select name, id, mainwindowtitle | ft -AutoSize"};
+				ProcessBuilder processBuilder = new ProcessBuilder(commandList);
+				Process list = processBuilder.start();
+				BufferedReader processBuffer = new BufferedReader(new InputStreamReader(list.getInputStream()));
+				String processLine = "";
+				while((processLine = processBuffer.readLine()) != null) {
+					System.out.println(processLine);
 				}
-			} 
+				
+			} catch (Exception e) {
+				e.getStackTrace();
+			}
+		} 
+
 			
 		} catch (Exception e) {
 			// TODO: handle exception
