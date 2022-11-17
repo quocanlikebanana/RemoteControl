0eb3557f57e817f32c2cd4d2959565f5fc8f50f2
An Ngo <quocankg2611@gmail.com>
2022-11-17 20:35:49 +0700

    Edit Server GUI

--- server_project/src/Application/Server_GUI.java
+++ server_project/src/Application/Server_GUI.java
@@ -26,9 +26,10 @@ import java.awt.event.ActionEvent;
 public class Server_GUI {
 
 	private JFrame frmServer;
-	private JButton openServer;
-	private JTextField textField;
-
+	private JTextField ipTxF;
+	private JButton openBtn;
+	private JButton closeBtn;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -50,6 +51,7 @@ public class Server_GUI {
 	 */
 	public Server_GUI() {
 		initialize();
+		AnNgoAddOns();
 	}
 
 	/**
@@ -61,68 +63,87 @@ public class Server_GUI {
 		frmServer.setTitle("Server");
 		frmServer.setAlwaysOnTop(true);
 		frmServer.setResizable(false);
-		frmServer.setBounds(100, 100, 597, 342);
+		frmServer.setBounds(100, 100, 542, 238);
 		frmServer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmServer.getContentPane().setLayout(null);
 		frmServer.getContentPane().setLayout(null);
 		
+		openBtn = new JButton("Open Server");
+		openBtn.setBounds(10, 10, 130, 40);
+		frmServer.getContentPane().add(openBtn);
+		openBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		openBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				openBtn.setVisible(false);
+				openBtn.setEnabled(false);
+				closeBtn.setVisible(true);
+				closeBtn.setEnabled(true);
+				ipTxF.setText("Anybody there...?");
+			}
+		});
+		openBtn.setForeground(Color.WHITE);
+		openBtn.setFont(new Font("Arial", Font.BOLD, 14));
+		openBtn.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		openBtn.setBackground(new Color(0, 102, 51));
+		
+		closeBtn = new JButton("Close Server");
+		closeBtn.setBounds(10, 10, 130, 40);
+		frmServer.getContentPane().add(closeBtn);
+		closeBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		closeBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				openBtn.setVisible(true);
+				openBtn.setEnabled(true);
+				closeBtn.setVisible(false);
+				closeBtn.setEnabled(false);
+				ipTxF.setText("None");
+			}
+		});
+		closeBtn.setForeground(Color.WHITE);
+		closeBtn.setFont(new Font("Arial", Font.BOLD, 14));
+		closeBtn.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		closeBtn.setBackground(new Color(153, 0, 0));
+		
 		JLabel lblNewLabel = new JLabel("Connected to:");
-		lblNewLabel.setFont(new Font("Cascadia Code", Font.BOLD, 16));
-		lblNewLabel.setBounds(170, 10, 131, 33);
+		lblNewLabel.setFont(new Font("Cascadia Code", Font.BOLD, 14));
+		lblNewLabel.setBounds(170, 10, 120, 33);
 		frmServer.getContentPane().add(lblNewLabel);
 		
 		JLabel lblActions = new JLabel("Actions:");
-		lblActions.setFont(new Font("Cascadia Code", Font.BOLD, 16));
-		lblActions.setBounds(64, 140, 96, 33);
+		lblActions.setFont(new Font("Arial", Font.BOLD, 14));
+		lblActions.setBounds(73, 75, 67, 33);
 		frmServer.getContentPane().add(lblActions);
 		
-		textField = new JTextField();
-		textField.setText("None");
-		textField.setFont(new Font("Cascadia Code", Font.PLAIN, 16));
-		textField.setEditable(false);
-		textField.setColumns(10);
-		textField.setBounds(311, 10, 232, 33);
-		frmServer.getContentPane().add(textField);
-		
-		JPanel panel = new JPanel();
-		panel.setBounds(10, 10, 150, 105);
-		frmServer.getContentPane().add(panel);
-		panel.setLayout(null);
-		panel.setBackground(SystemColor.inactiveCaption);
-		
-		JButton openServer_1 = new JButton("Open Server");
-		openServer_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		openServer_1.setForeground(Color.WHITE);
-		openServer_1.setFont(new Font("Arial", Font.BOLD, 16));
-		openServer_1.setBorderPainted(false);
-		openServer_1.setBorder(null);
-		openServer_1.setBackground(new Color(0, 102, 51));
-		openServer_1.setBounds(10, 10, 130, 40);
-		panel.add(openServer_1);
-		
-		JButton closeServer = new JButton("Close Server");
-		closeServer.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		closeServer.setForeground(Color.WHITE);
-		closeServer.setFont(new Font("Arial", Font.BOLD, 16));
-		closeServer.setBorderPainted(false);
-		closeServer.setBorder(null);
-		closeServer.setBackground(new Color(153, 0, 0));
-		closeServer.setBounds(10, 55, 130, 40);
-		panel.add(closeServer);
+		ipTxF = new JTextField();
+		ipTxF.setText("None");
+		ipTxF.setFont(new Font("Cascadia Code", Font.PLAIN, 14));
+		ipTxF.setEditable(false);
+		ipTxF.setColumns(10);
+		ipTxF.setBounds(300, 10, 192, 33);
+		frmServer.getContentPane().add(ipTxF);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(0, 125, 579, 5);
+		panel_1.setBounds(0, 60, 524, 5);
 		frmServer.getContentPane().add(panel_1);
 		panel_1.setBackground(new Color(55,70,91));
 		
 		JList list = new JList();
-		list.setBounds(170, 140, 373, 139);
+		list.setFont(new Font("Arial", Font.PLAIN, 14));
+		list.setBounds(170, 82, 322, 103);
 		frmServer.getContentPane().add(list);
+		
 	}
+	
+	/* An Ngo's add-ons */
+	private void AnNgoAddOns() {
+		ipTxF.setText("None");
+		
+		// Remove blue line wrap the text inside buttons
+		openBtn.setFocusPainted(false);
+		closeBtn.setFocusPainted(false);
+		
+		// By default
+		closeBtn.setEnabled(false);
+	}
+	
 }
--- server_project/src/Protocols/command.java
+++ server_project/src/Protocols/command.java
@@ -1,4 +1,4 @@
-package protocols;
+package Protocols;
 
 
 public final class command {
